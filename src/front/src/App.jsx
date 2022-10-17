11bb9d3859a1d86b9132528df765720719013f92
daiki-skm <daikisakuma6040@gmail.com>
2022-10-17 22:50:23 +0900

    add: web ar

--- src/front/src/App.jsx
+++ src/front/src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-// import { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
 
@@ -13,26 +12,51 @@ import "@ar-js-org/ar.js/aframe/build/aframe-ar-nft";
 // import { Entity, Scene } from "aframe-react";
 
 function App() {
+  // useEffect(() => {
+  //   let latitude, longitude;
+  //   navigator.geolocation.getCurrentPosition((data) => {
+  //     latitude = data.coords.latitude;
+  //     longitude = data.coords.longitude;
+  //   });
+  //   if (typeof document !== "undefined") {
+  //     const entity = document.getElementsByTagName("a-box");
+  //     console.log("before entity", entity[0]);
+  //     entity[0].setAttribute("gps-entity-place", {
+  //       latitude: latitude,
+  //       longitude: longitude,
+  //     });
+  //     entity[0].setAttribute("id", "demo");
+  //     console.log("after entity", entity[0]);
+  //   }
+  // });
+
+  let line = document.createElement("div");
+  const html = `
+  <a-scene
+    vr-mode-ui="enabled: false"
+    embedded
+    arjs="sourceType: webcam; debugUIEnabled: false;"
+  >
+    <a-text
+      value="This content will always face you."
+      look-at="[gps-camera]"
+      scale="30 30 30"
+      gps-entity-place="latitude: 37.492151723031024; longitude: 139.94461074269023;"
+    ></a-text>
+    <a-camera gps-camera rotation-reader></a-camera>
+  </a-scene>
+  `;
+  line.innerHTML = html;
+
   useEffect(() => {
-    let latitude, longitude;
-    navigator.geolocation.getCurrentPosition((data) => {
-      latitude = data.coords.latitude;
-      longitude = data.coords.longitude;
-    });
-    if (typeof document !== "undefined") {
-      const entity = document.getElementsByTagName("a-box");
-      console.log("before entity", entity[0]);
-      entity[0].setAttribute("gps-entity-place", {
-        latitude: latitude,
-        longitude: longitude,
-      });
-      entity[0].setAttribute("id", "demo");
-      console.log("after entity", entity[0]);
-    }
+    const tmp = document.getElementById("demo");
+    console.log("tmp", tmp);
+    tmp.appendChild(line);
   });
+
   return (
-    <div>
-      <a-scene
+    <div id="demo">
+      {/* <a-scene
         vr-mode-ui="enabled: false"
         arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
         renderer="antialias: true; alpha: true"
@@ -42,7 +66,7 @@ function App() {
       <div
         id="setloc"
         // style="position:absolute; left: 10px; bottom: 2%; z-index:999; background-color: blue; color: white; padding: 10px"
-      ></div>
+      ></div> */}
       {/* <a-scene arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-box
           material="color: yellow"
--- src/front/src/index.tsx
+++ src/front/src/index.tsx
@@ -7,6 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode>{/* <App /> */}</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
