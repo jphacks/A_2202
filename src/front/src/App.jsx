dec949e9c6be2ee7e8893a0999dacb10c47b6d98
daiki-skm <daikisakuma6040@gmail.com>
2022-10-18 13:55:29 +0900

    add: web ar

--- src/front/src/App.jsx
+++ src/front/src/App.jsx
@@ -1,38 +1,67 @@
-import React, { useEffect, useRef } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-
-import "aframe";
-import "aframe-look-at-component/dist/aframe-look-at-component";
-import "@ar-js-org/ar.js/aframe/build/aframe-ar-nft";
-
-// import "aframe-animation-component";
-// import "aframe-particle-system-component";
-// import "babel-polyfill";
-// import { Entity, Scene } from "aframe-react";
+import React, { useEffect } from "react";
+import AScene from "./components/AScene";
+import ACamera from "./components/ACamera";
+import AText from "./components/AText";
 
 function App() {
+  const commonProps = {
+    "look-At": "[gps-camera]",
+    "gps-entity-place":
+      "latitude: 37.492151723031024; longitude: 139.94461074269023;",
+    // 'latitude: 37.5150016; longitude: 139.9335767;',
+  };
+
+  useEffect(() => {
+    const entity = document.getElementsByTagName("canvas");
+    if (entity[0]) {
+      entity[0].addEventListener(
+        "click",
+        () => {
+          window.alert("aaaaaa");
+        },
+        false
+      );
+    }
+  });
+
   return (
-    <div id="demo">
-      {/* <a-scene
-        vr-mode-ui="enabled: false"
-        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
-        renderer="antialias: true; alpha: true"
+    <div style={{ width: "200vw", height: "100vh" }}>
+      <AScene
+        embedded=""
+        renderer="colorManagement: true"
+        vr-Mode-Ui="enabled: false"
+        arjs="trackingMethod: best; sourceType: webcam; matrixCodeType: 3x3; detectionMode:mono_and_matrix; debugUIEnabled: false;"
       >
-        <a-camera gps-new-camera="gpsMinDistance: 5"></a-camera>
-      </a-scene>
-      <div
-        id="setloc"
-        // style="position:absolute; left: 10px; bottom: 2%; z-index:999; background-color: blue; color: white; padding: 10px"
-      ></div> */}
-      {/* <a-scene arjs="sourceType: webcam; debugUIEnabled: false;">
-        <a-box
-          material="color: yellow"
-          look-at="[gps-camera]"
-          gps-entity-place="latitude: 37.5232359; longitude: 139.9393498"
+        <ACamera
+          gps-Camera="maxDistance: 0"
+          cursor="rayOrigin: mouse; fuse:false"
+          camera=""
         />
-        <div id="demo"></div>
-      </a-scene> */}
+
+        <AText
+          {...commonProps}
+          value={"Hello, World"}
+          scale={"8 8 8"}
+          position={"0 4 0"}
+          color={"black"}
+          width={18}
+          align="center"
+          z-Offset={1}
+        />
+
+        <AText
+          {...commonProps}
+          value={
+            "Hello, Demo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          }
+          scale={"1 1 1"}
+          position={"2 2 2"}
+          color={"white"}
+          width={18}
+          align="center"
+          z-Offset={2}
+        />
+      </AScene>
     </div>
   );
 }
