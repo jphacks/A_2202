a6e56cd37c430060230c4e35f629a87a52cb5d0a
hond0413 <hondsoccer0413@gmail.com>
2022-10-19 21:41:38 +0900

    feat: near realestate

--- src/back/go/handler/realestate.go
+++ src/back/go/handler/realestate.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"app/model"
 	"app/response"
 	"app/service"
 	"fmt"
+	"math"
 	"net/http"
 	"strconv"
 
@@ -15,6 +17,7 @@ type (
 		Index(c echo.Context) error
 		ByLatLon(c echo.Context) error
 		ByName(c echo.Context) error
+		ByNear(c echo.Context) error
 	}
 
 	RealEstate struct {
@@ -22,12 +25,16 @@ type (
 		geocodingService service.IGeocoding
 	}
 
-	JSONRealEstate struct {
+	JSONRealEstates struct {
 		Realestates *response.RealestateLatLonNames `json:"realestates`
 	}
 
 	JSONRealEstateDetail struct {
 		RealEstateDetail *response.RealestateDetails `json:"realestates`
+	}
+
+	JSONRealEstateNear struct {
+		RealEstateDetail *response.RealestateDetail `json:"realestates`
 	}
 )
 
@@ -51,31 +58,7 @@ func (h *RealEstate) Index(c echo.Context) error {
 }
 
 func (h *RealEstate) ByLatLon(c echo.Context) error {
-	queryLat := c.QueryParam("latitude")
-	if len(queryLat) == 0 {
-		return c.JSON(http.StatusBadRequest, "latitude is required")
-	}
-	queryLon := c.QueryParam("longitude")
-	if len(queryLon) == 0 {
-		return c.JSON(http.StatusBadRequest, "longitude is required")
-	}
-	latitude, err := strconv.ParseFloat(queryLat, 64)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, "latitude type is invalid")
-	}
-	longitude, err := strconv.ParseFloat(queryLon, 64)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, "longitude type is invalid")
-	}
-	geoCode, err := h.geocodingService.GetGeocode(latitude, longitude)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
-	}
-
-	prefecture := geoCode.Results[0].AddressComponents[len(geoCode.Results[0].AddressComponents) - 3].LongName
-	city := geoCode.Results[0].AddressComponents[len(geoCode.Results[0].AddressComponents) - 4].LongName
-
-	realestates, err := h.realestateService.GetByPreCit(prefecture, city)
+	realestates, err := h.byLatLonRealestats(c)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
 	}
@@ -89,7 +72,7 @@ func (h *RealEstate) ByLatLon(c echo.Context) error {
 		(*realestates)[i].Longitude = lagLng.Results[0].Geometry.Location.Lng
 	}
 
-	return c.JSON(http.StatusOK, &JSONRealEstate{
+	return c.JSON(http.StatusOK, &JSONRealEstates{
 		Realestates: response.NewRealestateLatLonNames(realestates),
 	})
 }
@@ -108,4 +91,78 @@ func (h *RealEstate) ByName(c echo.Context) error {
 	return c.JSON(http.StatusOK, &JSONRealEstateDetail{
 		RealEstateDetail: response.NewRealEstateDetails(realestates),
 	})
+}
+
+func (h *RealEstate) ByNear(c echo.Context) error {
+	queryLat := c.QueryParam("latitude")
+	if len(queryLat) == 0 {
+		return c.JSON(http.StatusBadRequest, "latitude is required")
+	}
+	queryLon := c.QueryParam("longitude")
+	if len(queryLon) == 0 {
+		return c.JSON(http.StatusBadRequest, "longitude is required")
+	}
+	latitude, err := strconv.ParseFloat(queryLat, 64)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
+	}
+	longitude, err := strconv.ParseFloat(queryLon, 64)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
+	}
+
+	realestates, err := h.byLatLonRealestats(c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%v", err))
+	}
+
+	(*realestates)[0].Distance = h.byDistance(latitude, longitude, (*realestates)[0].Latitude, (*realestates)[0].Longitude)
+	nearRealestate := (*realestates)[0]
+	for i, v := range *realestates {
+		(*realestates)[i].Distance = h.byDistance(latitude, longitude, v.Latitude, v.Longitude)
+		if (*realestates)[i].Distance < nearRealestate.Distance {
+			nearRealestate = (*realestates)[i]
+		}
+	}
+
+	return c.JSON(http.StatusOK, &JSONRealEstateNear{
+		RealEstateDetail: response.NewRealEstateDetail(&nearRealestate),
+	})
+}
+
+func (h *RealEstate) byLatLonRealestats(c echo.Context) (*model.RealEstates, error) {
+	queryLat := c.QueryParam("latitude")
+	if len(queryLat) == 0 {
+		return nil, fmt.Errorf("latitude is required")
+	}
+	queryLon := c.QueryParam("longitude")
+	if len(queryLon) == 0 {
+		return nil, fmt.Errorf("longitude is required")
+	}
+	latitude, err := strconv.ParseFloat(queryLat, 64)
+	if err != nil {
+		return nil, fmt.Errorf("latitude type is invalid")
+	}
+	longitude, err := strconv.ParseFloat(queryLon, 64)
+	if err != nil {
+		return nil, fmt.Errorf("longitude type is invalid")
+	}
+	geoCode, err := h.geocodingService.GetGeocode(latitude, longitude)
+	if err != nil {
+		return nil, err
+	}
+
+	prefecture := geoCode.Results[0].AddressComponents[len(geoCode.Results[0].AddressComponents) - 3].LongName
+	city := geoCode.Results[0].AddressComponents[len(geoCode.Results[0].AddressComponents) - 4].LongName
+
+	realestates, err := h.realestateService.GetByPreCit(prefecture, city)
+	if err != nil {
+		return nil, err
+	}
+
+	return realestates, nil
+}
+
+func (h *RealEstate) byDistance(x1, y1, x2, y2 float64) float64 {
+	return math.Acos(math.Sin(y1)*math.Sin(y2)+math.Cos(y1)*math.Cos(y2)*math.Cos(x2-x1))
 }
