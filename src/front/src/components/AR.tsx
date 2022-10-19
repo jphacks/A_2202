2c189ecd2930b83ec51bb9cd989a24c125385e20
daiki-skm <daikisakuma6040@gmail.com>
2022-10-19 15:52:59 +0900

    fix: gps

--- src/front/src/components/AR.tsx
+++ src/front/src/components/AR.tsx
@@ -1,28 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AScene from "./aframe/AScene";
 import ACamera from "./aframe/ACamera";
 import AText from "./aframe/AText";
 
 const AR = () => {
+  const [latitude, setLatitude] = useState<number>();
+  const [longitude, setLongitude] = useState<number>();
+
+  // TODO: latitude, longitudeをポーリング？
+  // しないのであれば、useState消す
+  // const el = document.querySelector("[gps-entity-place]");
+  // console.log("el", el);
+  // el?.addEventListener("gps-entity-place-update-positon", (event) => {
+  //   console.log("event", event);
+  //   // if (event.detail.distance < 100) {
+  //   //   el.setAttribute("material", "color: yellow");
+  //   // } else {
+  //   //   el.setAttribute("material", "color: red");
+  //   // }
+  // });
+
+  if (!navigator.geolocation) {
+    window.alert("Your browser doesn't support Geolocation");
+  } else {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+        console.log("lat, lon", latitude, longitude);
+      },
+      () => {
+        window.alert("Failed to get your location!");
+      }
+    );
+  }
+
   const commonProps = {
     "look-At": "[gps-camera]",
     "gps-entity-place":
       "latitude: 37.492151723031024; longitude: 139.94461074269023;",
+    // `latitude: ${latitude}; longitude: ${longitude};`,
     // "latitude: 37.5150016; longitude: 139.9335767;",
   };
-
-  // useEffect(() => {
-  //   const entity = document.getElementsByTagName("canvas");
-  //   if (entity[0]) {
-  //     entity[0].addEventListener(
-  //       "click",
-  //       () => {
-  //         window.alert("aaaaaa");
-  //       },
-  //       false
-  //     );
-  //   }
-  // });
 
   return (
     <div style={{ width: "200vw", height: "100vh" }}>
@@ -42,22 +61,12 @@ const AR = () => {
 
         <AText
           {...commonProps}
-          value={"Hello, World"}
+          value={`${latitude}, ${longitude} \n Hello, World! aaaaaaaaaaaaaaaaaaaaaaaaaaa`}
           scale={"0.5 0.5 0.5"}
-          color={"black"}
+          color={"red"}
           width={18}
+          // z-Offset={0}
           // align="center"
-          // z-Offset={1}
-        />
-
-        <AText
-          {...commonProps}
-          value={
-            "Hello, Demo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-          }
-          scale={"0.5 0.5 0.5"}
-          color={"white"}
-          width={18}
         />
       </AScene>
     </div>
