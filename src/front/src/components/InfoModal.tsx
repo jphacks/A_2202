508ebd1362481f2e81c1851cac605465d69598a7
tom1236908745 <tom1233908745@gmail.com>
2022-10-21 10:38:38 +0900

    fix: moddalの修正

--- src/front/src/components/InfoModal.tsx
+++ src/front/src/components/InfoModal.tsx
@@ -6,30 +6,33 @@ import ModalClose from "@mui/joy/ModalClose";
 import ModalDialog from "@mui/joy/ModalDialog";
 import Typography from "@mui/joy/Typography";
 
-const InfoModal = () => {
+const InfoModal: React.FC<{
+  latitude: number;
+  longitude: number;
+}> = ({ latitude, longitude }) => {
   const [open, setOpen] = useState(false);
   const [realEstateDetail, setRealEstateDetail] = useState<
     [] | RealEstateDetail[]
   >([]);
 
-  const getRealEstateDetail = async () => {
-    // const url =
-    //   "https://back-lpzceixskq-de.a.run.app/realestate/detail/str?name=ネオグランデ上町";
-    const url =
-      "http://localhost:8080/realestate/detail/str?name=ネオグランデ上町";
+  const getRealEstateDetail = async (latitude: number, longitude: number) => {
+    // const url = `https://back-lpzceixskq-de.a.run.app/realestate/detail/latlon?latitude=${latitude}&longitude=${longitude}`;
+    const url = `http://localhost:8080/realestate/detail/latlon?latitude=${latitude}&longitude=${longitude}`;
+    // window.alert("InfoModal url" + url);
     await fetch(url)
       .then((res: any) => res.json())
       .then((data) => {
-        setRealEstateDetail(data.RealEstateDetail);
+        setRealEstateDetail(data.RealEstateDetails);
       })
       .catch((err) => {
-        console.error("ERROR API: ", err);
-      });
+        window.alert("Failed to get API!");
+        // window.alert("InfoModal data.Realestates" + data.Realestates);
+      })
   };
 
   useEffect(() => {
-    getRealEstateDetail();
-  }, []);
+    getRealEstateDetail(latitude, longitude);
+  }, [latitude, longitude]);
 
   return (
     <div>
@@ -52,17 +55,216 @@ const InfoModal = () => {
             component="h2"
             level="inherit"
             fontSize="1.25em"
-            mb="0.25em"
+            mb="1.25em"
           >
             物件情報
           </Typography>
-          <Typography id="variant-modal-description" textColor="inherit">
-            {Array.isArray(realEstateDetail)
-              ? realEstateDetail.map((element) => {
-                  return <span key={element.id}>{element.name}</span>;
-                })
-              : "Fail"}
-          </Typography>
+          
+            {realEstateDetail.length !== 0
+              ?
+              <div>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  建物名
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].name}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  種類
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].property_type} 円
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  値段
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].price}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  賃貸料
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].rent}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  契約形態
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].transaction_contents}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  敷地面積
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].area}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  設立年
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].year_builds}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  住所
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].address}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  キャッチコピー
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].catch_copy}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  部屋数
+                </Typography>
+                < div className="m-4 p-1">Hello World</ div>
+                {/* 
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].room_count}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  フロア数
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].floor_plan}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  方角
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].direction}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  構造
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].propety_storucture}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  階数
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].total_ground_story}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  地下階数
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].under_ground_story}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].total_unit}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography> */}
+                {/* <Typography textColor="inherit">
+                  {realEstateDetail[0].fee}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].facility}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].neighbor}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].current_status}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].latitude}
+                </Typography>
+                <Typography
+                  fontSize="1.2em"
+                  mb="0.25em"
+                >
+                  物件情報
+                </Typography>
+                <Typography textColor="inherit">
+                  {realEstateDetail[0].longitude}
+                </Typography> */}
+              </div>
+              : "No Data"}
         </ModalDialog>
       </Modal>
     </div>
--- src/front/tailwind.config.js
+++ src/front/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
-    extend: {},
+  extend: {},
+  },
+  variants: {
+  extend: {},
   },
   plugins: [],
 };
